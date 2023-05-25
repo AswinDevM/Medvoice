4d1bfb3072fd31f9949cd530ca6294eb77ae5b57
AswinDevM <117139462+AswinDevM@users.noreply.github.com>
2023-05-25 20:04:38 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$db_name = "register";
+$conn = mysqli_connect($hostname,$username,$password,$db_name);
+if(!$conn)
+{
+   echo"connection failed";
+}
+?>
